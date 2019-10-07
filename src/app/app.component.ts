cb368e9ff1d77b1d74370f2a732a2b972c1cfa03
Alonso Cadenas <alonso.cadenas@gartner.com>
2019-10-07 12:49:05 -0500

    Improve mocks and find method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,15 @@ import {Table} from './classes/table';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  tableId: number;
+  foundTable: Table;
   tables: Table[] = [
     new Table(1, ['John Doe', 'Jane Doe']),
     new Table(2, ['Juan Gomez', 'Maria Gomez']),
-    new Table(3, ['Luis Diaz', 'Aaron Messi']),
-    new Table(4, ['Lionel Pena', 'Cristiano Giggs']),
+    new Table(3, ['Luis Suarez', 'Ryan Giggs']),
+    new Table(4, ['Lionel Messi', 'Cristiano Ronaldo']),
   ];
 
   find(name) {
-    const found: Table = this.tables.find(table => table.findByName(name));
-    this.tableId = found ? found.id : undefined;
+    this.foundTable = this.tables.find(table => table.findByName(name));
   }
 }
